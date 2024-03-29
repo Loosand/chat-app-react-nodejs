59d48803c4a206818164789390277a3bc84f5f76
Loosand <loosand@163.com>
2024-03-29 21:10:39 +0800

    稳定版本

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
-
-const app = express()
+const authRoutes = require("./routes/userRouter");
+const messageRoutes = require("./routes/messagesRouter");
+const app = express();
+const socket = require("socket.io")
 require("dotenv").config()
-
 
 app.use(cors())
 app.use(express.json())
@@ -20,6 +21,32 @@ mongoose.connect(process.env.MONGO_URL, {
     console.log(err.message)
   })
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
+})
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true
+  }
+})
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on('add-user', (userId) => {
+    onlineUsers.set(userId, socket.id);
+  })
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  })
 })
